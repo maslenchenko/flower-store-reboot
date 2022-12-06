2ca6ce93ef1e464cdd83a13118d1b15c94dcc2c7
oleksandra <91615687+maslenchenko@users.noreply.github.com>
2022-12-06 21:43:10 +0200

    Update DemoFlowerStoreApplicationTests.java

--- src/test/java/com/example/demoFlowerStore/DemoFlowerStoreApplicationTests.java
+++ src/test/java/com/example/demoFlowerStore/DemoFlowerStoreApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.demoFlowerStore;
+// package com.example.demoFlowerStore;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DemoFlowerStoreApplicationTests {
+// @SpringBootTest
+// class DemoFlowerStoreApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
